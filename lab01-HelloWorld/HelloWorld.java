114d969e4604c3805eadba40b98f53e932035c6a
ktg314 <ktg314@lehigh.edu>
2015-08-28 15:22:30 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+//////////////////////////////////
+//Ken Grenstedt
+//Demo 0
+//Hellow World Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+//  define a class
+public class HelloWorld{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the statement with box around
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+        //test/testtest
+        
+    }
+}
