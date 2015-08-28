384dd4961beba1913a910a2120f95c52d8aaca38
ktg314 <ktg314@lehigh.edu>
2015-08-28 15:43:32 +0000

    first version of WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,33 @@
+//////////////////////////////////
+//Ken Grenstedt
+//hw #1
+//Welcome Class Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+        
+//  print the tweet-length autobiographic statement
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-K--T--G--3--1--4->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This August my wife and I took an eleven-day, 675-mile bike");
+        System.out.println("tour on our tandem. We circled Lake Ontario.");
+        
+        
+        //test//testtest1
+        
+    }
+}
