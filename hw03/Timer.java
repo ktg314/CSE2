b590ed3824eaef7722a55173cbd2411c8bfcad8e
ktg314 <ktg314@lehigh.edu>
2015-09-14 14:56:25 +0000

    added extra comments to hw03, check.java

--- hw03/Timer.java
+++ hw03/Timer.java
@@ -26,13 +26,16 @@ public class Timer {   //open class
         Scanner myScanner;
         myScanner = new Scanner( System.in); //declaring scanner
         
+        //ask for current time in form of ABCD (integer)
         System.out.print("Enter the current time: ");
         int timeCurrent = myScanner.nextInt();
 
+        //ask for dinner time in form of WXYZ (integer)
         System.out.print("Enter the time that you will be eating dinner: ");
         int timeDinner = myScanner.nextInt();
         
         /////////Calculations
+        //define variables
         int remainingHours,
             remainingMinutes,
             hoursCurrent,
@@ -40,12 +43,18 @@ public class Timer {   //open class
             hoursDinner,
             minutesDinner;
         
+        //get rid of 1000ths and 100ths place
         minutesCurrent = timeCurrent%100;
+        //get remaing 10^3 and 10^4th numbers representing hours then divide by 100
         hoursCurrent = (timeCurrent - minutesCurrent)/100;
         
+        //get rid of 1000ths and 100ths place
         minutesDinner = timeDinner%100;
+        //get remaing 10^3 and 10^4th numbers representing hours then divide by 100
         hoursDinner = (timeDinner - minutesDinner)/100;
         
+        
+        //simple subtraction to get time difference until dinner
         remainingHours = hoursDinner - hoursCurrent;
         remainingMinutes = minutesDinner - minutesCurrent;
         
